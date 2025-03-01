c6bbd2acd2fe163d62c11c7d881dc72cd5d52dec
oliviaaasher <oliviaallisonasher@gmail.com>
2025-03-01 15:48:09 -0600

    update

--- Maps.r
+++ Maps.r
@@ -6,10 +6,9 @@ library(leaflet)  #interactive maps
 library(htmltools) # Add html popups
 
 # Load CSV
-directory <- "data/"
-birds_poultry <- read.csv(paste0(directory, "bird_flu_poultry.csv"))
-
-birds_wildbirds <- read.csv(paste0(directory, "bird_flu_wildbirds.csv"))
+directory <- "/Users/oliviaasher/Library/Mobile Documents/com~apple~CloudDocs/bird_flu"
+birds_poultry <- read.csv(paste0(directory, "/bird_flu_poultry.csv"))
+birds_wildbirds <- read.csv(paste0(directory, "/bird_flu_wildbirds.csv"))
 
 # Get US county shapefile from `tigris`
 counties <- counties(cb = TRUE, resolution = "20m") %>%
@@ -17,6 +16,9 @@ counties <- counties(cb = TRUE, resolution = "20m") %>%
 
 # Ensure FIPS is a character for merging
 birds_poultry$FIPS <- as.character(birds_poultry$FIPS)
+birds_wildbirds$FIPS <- as.character(birds_wildbirds$FIPS)
+
+colnames(birds_wildbirds) <- c("FullGeoName.Wild", "FIPS", "State.Wild", "County.Wild", "Date.Detected.Wild", "Strain.Type.Source.Wild", "States.Wild", "Counties.Wild", "Totals.Wild")
 
 # Merge your data with county geometries
 geo_data <- merge(counties, birds_poultry, by = "FIPS")
@@ -25,12 +27,7 @@ geo_data <- st_transform(geo_data, crs = '+proj=longlat +datum=WGS84')
 
 # Continuous palette
 pal <- colorNumeric(palette = "viridis", domain = geo_data$Flock.Size)
-p_popup <- paste0("<strong>Outbreak number: </strong>", 
-                  geo_data$Outbreaks, 
-                  "<br><strong>Flock Type: </strong>", 
-                  geo_data$Flock.Type, 
-                  "<br><strong>Outbreak Date: </strong>",
-                  geo_data$Outbreak.Date)
+p_popup <- paste0("<strong>Outbreak number: </strong>", geo_data$Outbreaks, "<br><strong>Flock Type: </strong>", geo_data$Flock.Type, "<br><strong>Outbreak Date: </strong>",geo_data$Outbreak.Date)
 
 map <- leaflet() %>%
   addTiles() %>%
@@ -47,19 +44,3 @@ map <- leaflet() %>%
   addLegend("bottomleft", pal = pal, values = geo_data$Flock.Size)
 
 
-<<<<<<< HEAD
-=======
-map %>% addLegend('bottomleft', pal = pal, values = geo_data$Flock.Size)
-map 
-
-
-
-# Statistical Analysis ----
-# Calculate Pearson correlation between Flock Size and Outbreak
-pearson_correlation <- cor(geo_data$Flock.Size, geo_data$Outbreaks, method = "pearson")
-
-contingency_table <- table(geo_data$Flock.Size, geo_data$Outbreaks)
-
-# Perform Chi-Square test
-chi_square_test <- chisq.test(contingency_table)
->>>>>>> refs/remotes/origin/main

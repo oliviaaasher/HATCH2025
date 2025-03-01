2badb33fb66f2841c1131e6b3280603acc3b01f4
oliviaaasher <oliviaallisonasher@gmail.com>
2025-03-01 15:03:13 -0600

    updating

--- Maps.r
+++ Maps.r
@@ -11,24 +11,24 @@ birds_poultry <- read.csv(paste0(directory, "/bird_flu_poultry.csv"))
 birds_wildbirds <- read.csv(paste0(directory, "/bird_flu_wildbirds.csv"))
 
 # Get US county shapefile from `tigris`
-counties <- counties(cb = TRUE, resolution = "20m") %>% 
+counties <- counties(cb = TRUE, resolution = "20m") %>%
   rename(FIPS = GEOID)  # Match FIPS column for merging
 
 # Ensure FIPS is a character for merging
-birds_wildbirds$FIPS <- as.character(birds_wildbirds$FIPS)
+birds_poultry$FIPS <- as.character(birds_poultry$FIPS)
 
 # Merge your data with county geometries
-geo_data <- merge(counties, birds_wildbirds, by = "FIPS")
+geo_data <- merge(counties, birds_poultry, by = "FIPS")
 
 geo_data <- st_transform(geo_data, crs = '+proj=longlat +datum=WGS84')
 
 # Continuous palette
 pal <- colorNumeric(palette = "viridis", domain = geo_data$Flock.Size)
-p_popup <- paste0("<strong>Outbreak number: </strong>", geo_data$Outbreaks, "<br\><strong>Outbreak number: </strong>",, geo_data$Flock.Type)
+p_popup <- paste0("<strong>Outbreak number: </strong>", geo_data$Outbreaks, "<br><strong>Flock Type: </strong>", geo_data$Flock.Type, "<br><strong>Outbreak Date: </strong>",geo_data$Outbreak.Date)
 
 map <- leaflet() %>%
        addTiles() %>%
        setView(lng = -98.5795, lat = 39.8283, zoom = 4) %>%
-       addPolygons(data = geo_data, color = pal(geo_data$Flock.Size), stroke = 1, opacity = 0.8, popup = p_popup)
+       addPolygons(data = geo_data, color = pal(geo_data$Flock.Size), stroke = 0.1, opacity = 0.8, popup = p_popup)
 
 map %>% addLegend('bottomleft', pal = pal, values = geo_data$Flock.Size)
